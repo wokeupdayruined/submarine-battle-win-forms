5af337c6b66e2c8126098fee24e7632343ce4894
Abraamyan Alexander <samvelcc25@outlook.com>
2025-03-15 13:14:31 +0300

    initial

---
+++ BattleShip.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sea_battle_C_
+{
+    enum Direction { Right, Left, Down, Up };
+    internal class BattleShip : Control
+    {
+        Bitmap Bitmap { get; set; }
+        public int MaxX { get; set; }
+        public int MaxY { get; set; }
+        public int XSpeed { get; set; } = 5;
+        public int YSpeed { get; set; } = 5;
+        public Direction Direction { get; set; }
+        public BattleShip()
+        {
+            Bitmap = new Bitmap("resources/ship.jpg");
+            Bitmap = new Bitmap(Bitmap, new Size(40, 40));
+        }
+
+        public void MoveShip()
+        {
+            switch (Direction)
+            {
+                case Direction.Right:
+                    MoveRight();
+                    break;
+                case Direction.Left:
+                    MoveLeft();
+                    break;
+                case Direction.Down:
+                    MoveDown();
+                    break;
+                case Direction.Up:
+                    MoveUp();
+                    break;
+            }
+        }
+
+        public void MoveRight()
+        {
+            var location = Location;
+            location.X = Math.Min(location.X + XSpeed, MaxX - Width);
+            Location = location;
+        }
+
+        public void MoveLeft()
+        {
+            var location = Location;
+            location.X = Math.Max(location.X - XSpeed, 0);
+            Location = location;
+        }
+
+        public void MoveDown()
+        {
+            var location = Location;
+            location.Y = Math.Min(location.Y + YSpeed, MaxY - Height);
+            Location = location;
+        }
+
+        public void MoveUp()
+        {
+            var location = Location;
+            location.Y = Math.Max(location.Y - YSpeed, 0);
+            Location = location;
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            var graphics = e.Graphics;
+            graphics.DrawImageUnscaled(Bitmap, 0, 0);
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sea_battle_C_
+{
+    public partial class Form1 : Form
+    {
+        BattleShip ship = new BattleShip();
+        Timer timer = new Timer();
+        public Form1()
+        {
+            InitializeComponent();
+            Controls.Add(ship);
+            ship.MaxX = Width;
+            ship.MaxY = Height;
+            ship.Size = new System.Drawing.Size(40, 40);
+            this.KeyPreview = true;
+            this.KeyDown += new KeyEventHandler(this.Form1_KeyDown);
+            timer.Interval = 1000/60;
+            timer.Tick += new EventHandler(StepTimer);
+            timer.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void StepTimer(object sender, EventArgs e)
+        {
+            ship.MoveShip();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.D)
+            {
+                ship.Direction = Direction.Right;
+            } else if (e.KeyCode == Keys.S)
+            {
+                ship.Direction = Direction.Down;
+            } else if (e.KeyCode == Keys.A)
+            {
+                ship.Direction = Direction.Left;
+            }
+            else if (e.KeyCode == Keys.W)
+            {
+                ship.Direction = Direction.Up;
+            }
+        }
+    }
+}
